481833cee6fcd57421f82384546b23a68b406b9c
kryman27 <77673262+kryman27@users.noreply.github.com>
2023-06-05 21:03:45 +0200

    #1-Frontend Early version of application's front-end layer prepared

---
+++ MealPlannerUI/Data/ProductInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace MealPlannerUI.Data
+{
+    public class ProductInfo
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; } = null!;
+
+        public int? Category { get; set; }
+
+        public int? Brand { get; set; }
+
+        public double? Fat { get; set; }
+
+        public double? Carbohydrates { get; set; }
+
+        public double? Protein { get; set; }
+
+        public double? Energy { get; set; }
+    }
+}
---
+++ MealPlannerUI/Data/ProductInfoService.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace MealPlannerUI.Data
+{
+    public class ProductInfoService
+    {
+        public async Task<ProductInfo[]> GetProductsInfo()
+        {
+            //List<ProductInfo> products = new List<ProductInfo>();
+            HttpClient client = new HttpClient();
+
+            string productsApiUrl = "http://localhost:5068/GetProducts";
+            //var response = client.GetAsync(connectionPath).Result.ToString();
+            //var result = JsonConvert.DeserializeObject<ProductInfo>(response);
+
+            //return result.ToString();
+
+            HttpResponseMessage response = await client.GetAsync(productsApiUrl);
+            string rawData = await response.Content.ReadAsStringAsync();
+
+            var productInfoToDisplay = JsonConvert.DeserializeObject<ProductInfo[]>(rawData);
+
+            client.CancelPendingRequests();
+            client.Dispose();
+
+            return productInfoToDisplay;
+        }
+    }
+}
