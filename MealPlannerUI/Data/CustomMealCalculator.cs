2881a5b1c5d0599b61c71275ac360fe6a14ec787
kryman27 <77673262+kryman27@users.noreply.github.com>
2023-08-08 22:35:51 +0200

    #3-MealTable adding custom meal functionality works, modification in CsvParser has been done, dbo.Products table has been corrected

--- MealPlannerUI/Data/CustomMealCalculator.cs
+++ MealPlannerUI/Data/CustomMealCalculator.cs
@@ -1,11 +1,23 @@
-﻿namespace MealPlannerUI.Data
+﻿using MealPlannerUI.Pages;
+
+namespace MealPlannerUI.Data
 {
     public class CustomMealCalculator : ProductInfo
     {
         public double Amount { get; set; }
-        public static ProductInfo CalculateCustomMeal(List<CustomMealCalculator> givenList)
+        public static void CalculateCustomMeal(List<CustomMealCalculator> givenList)
         {
-            return new ProductInfo();
+            double totalMass = givenList.Sum(p => p.Amount);
+            double? totalFat = givenList.Sum(p => p.Fat);
+            double? totalCarbs = givenList.Sum(p => p.Carbohydrates);
+            double? totalProtein = givenList.Sum(p => p.Protein);
+            double? totalEnergy = givenList.Sum(p => p.Energy);
+
+            CreateCustomMeal.customProduct.Carbohydrates = Math.Round((double)((totalCarbs * 100)/totalMass), 2);
+            CreateCustomMeal.customProduct.Fat = Math.Round((double)((totalFat * 100) / totalMass), 2);
+            CreateCustomMeal.customProduct.Protein = Math.Round((double)((totalProtein * 100) / totalMass), 2);
+            CreateCustomMeal.customProduct.Energy = Math.Round((double)((totalEnergy * 100) / totalMass), 2);
+
         }
     }
 }
