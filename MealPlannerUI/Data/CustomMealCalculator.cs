20c5cf311efbd032b51690d2d6a01fcbbed1cefb
kryman27 <77673262+kryman27@users.noreply.github.com>
2023-08-09 14:41:52 +0200

    #3-MealTables logic for calculations fixed, csv parser fixed, MudBlazor components library added to the project

--- MealPlannerUI/Data/CustomMealCalculator.cs
+++ MealPlannerUI/Data/CustomMealCalculator.cs
@@ -7,16 +7,17 @@ namespace MealPlannerUI.Data
         public double Amount { get; set; }
         public static void CalculateCustomMeal(List<CustomMealCalculator> givenList)
         {
-            double totalMass = givenList.Sum(p => p.Amount);
+            double? totalMass = givenList.Sum(p => p.Amount);
             double? totalFat = givenList.Sum(p => p.Fat);
             double? totalCarbs = givenList.Sum(p => p.Carbohydrates);
             double? totalProtein = givenList.Sum(p => p.Protein);
             double? totalEnergy = givenList.Sum(p => p.Energy);
 
-            CreateCustomMeal.customProduct.Carbohydrates = Math.Round((double)((totalCarbs * 100)/totalMass), 2);
-            CreateCustomMeal.customProduct.Fat = Math.Round((double)((totalFat * 100) / totalMass), 2);
-            CreateCustomMeal.customProduct.Protein = Math.Round((double)((totalProtein * 100) / totalMass), 2);
-            CreateCustomMeal.customProduct.Energy = Math.Round((double)((totalEnergy * 100) / totalMass), 2);
+            CreateCustomMeal.customProduct.Carbohydrates = Math.Round((double)((totalCarbs * 100.0)/totalMass), 2);
+            CreateCustomMeal.customProduct.Fat = Math.Round((double)((totalFat * 100.0) / totalMass), 2);
+            CreateCustomMeal.customProduct.Protein = Math.Round((double)((totalProtein * 100.0) / totalMass), 2);
+            CreateCustomMeal.customProduct.Energy = Math.Round((double)((totalEnergy * 100.0) / totalMass), 2);
+            //CreateCustomMeal.customProduct.Energy = ((totalEnergy * 100.0) / totalMass);
 
         }
     }
--- MealPlannerUI/Program.cs
+++ MealPlannerUI/Program.cs
@@ -1,6 +1,7 @@
 using MealPlannerUI.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<ProductInfoService>();
 builder.Services.AddSingleton<AddProductService>();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
